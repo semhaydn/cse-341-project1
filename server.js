504f9e8f2fcfb7c118bff8d328b1b4db0dc08f70
Semhaydn <ayd23002@byui.edu>
2024-06-30 03:57:31 +0300

    port update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Server is running on port ${PORT}`)});
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
 
 app.use('/', require('./routes'));
