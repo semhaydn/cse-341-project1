100c64cc01f27ea02b4dd67cb2edf969994226e5
Semhaydn <ayd23002@byui.edu>
2024-06-30 05:52:51 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World');
-});
+router.use('/', require('./users'))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
-
 app.use('/', require('./routes'));
+
+mongodb.initDb ((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
+    }
+});
